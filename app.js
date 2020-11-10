d0a842e510d6796cc18f3ef01ac3dbba4eb5811f
r-asada-ab <max_min_ab@outlook.jp>
2020-11-10 13:09:27 +0900

    mongooseを使用するよう修正

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// db
+var mongoDB = 'mongodb://127.0.0.1:27017/app_db'
+mongoose.connect(mongoDB)
+mongoose.Promise = global.Promise
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
