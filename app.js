a3c65323f5b5f1a17458f5d1187ded2600a1e3e6
r-asada-ab <max_min_ab@outlook.jp>
2020-11-27 08:27:44 +0900

    差分生成のためリファクタリング

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ mongoose.connect("mongodb://"+process.env.COSMOSDB_HOST+":"+process.env.COSMOSDB
     user: process.env.COSMOSDB_USER,
     password: process.env.COSMOSDB_PASSWORD
   },
-useNewUrlParser: true,
-useUnifiedTopology: true,
-retryWrites: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  retryWrites: false
 })
 .then(() => console.log('Connection to CosmosDB successful'))
 .catch((err) => console.error(err));
