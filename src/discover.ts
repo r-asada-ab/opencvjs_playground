b996e954020ea66381f661323ec609be120aa167
r-asada-ab <max_min_ab@outlook.jp>
2020-11-29 19:09:05 +0900

    検索したときの以前の検索結果を削除するよう修正

--- src/discover.ts
+++ src/discover.ts
@@ -1,10 +1,51 @@
+// テーブルをリセットする
+function resetTable() {
+    let tBody = document.getElementById("table_body")
+    while (tBody.hasChildNodes()) {
+        tBody.removeChild(tBody.lastChild);
+    }
+}
+
+// テーブルを作成する
+function createTable(body) {
+    let tBody = document.getElementById("table_body")
+    for (let i = 0; i < body.length; i++) {
+        let b = body[i]
+        let tr = document.createElement("tr")
+        let title = document.createElement("td")
+        let discription = document.createElement("td")
+        let snipet = document.createElement("td")
+        let date = document.createElement("td")
+
+        title.innerText = b.title
+        discription.innerText = b.discription
+        let s = b.snipet.slice(0, 20) + "..."
+        snipet.innerText = s
+        let d = new Date(b.date)
+        date.innerText = d.toDateString()
+
+        tr.append(title)
+        tr.append(discription)
+        tr.append(snipet)
+        tr.append(date)
+
+        tr.addEventListener("click", () => {
+            location.href = "index.html?id=" + encodeURIComponent(b._id)
+        })
+
+        tBody.append(tr)
+    }
+}
+
 function main() {
 
+    // タイトルを押したとき
     let title = document.getElementById("title_text")
     title.addEventListener("click", () => {
         location.href = "index.html"
     })
 
+    // 検索ボタンを押したとき
     let searchButton = document.getElementById("search_button")
     searchButton.addEventListener("click", () => {
         let searchBox = <HTMLInputElement>document.getElementById("textbox")
@@ -22,34 +63,8 @@ function main() {
     
         promise.then(async (resolve) => {
             let body = await resolve.json()
-
-            let tBody = document.getElementById("table_body")
-            for (let i = 0; i < body.length; i++) {
-                let b = body[i]
-                let tr = document.createElement("tr")
-                let title = document.createElement("td")
-                let discription = document.createElement("td")
-                let snipet = document.createElement("td")
-                let date = document.createElement("td")
-
-                title.innerText = b.title
-                discription.innerText = b.discription
-                let s = b.snipet.slice(0, 20) + "..."
-                snipet.innerText = s
-                let d = new Date(b.date)
-                date.innerText = d.toDateString()
-    
-                tr.append(title)
-                tr.append(discription)
-                tr.append(snipet)
-                tr.append(date)
-
-                tr.addEventListener("click", () => {
-                    location.href = "index.html?id=" + encodeURIComponent(b._id)
-                })
-    
-                tBody.append(tr)
-            }
+            resetTable()
+            createTable(body)
         })
     })
 }
